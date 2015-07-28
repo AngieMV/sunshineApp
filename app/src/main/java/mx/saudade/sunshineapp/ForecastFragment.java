70a0d6a909013b9644ee4b22c0680e1502befb70
Angelica Mendez Vega <angelicamendezvega@fabarca.cameiap.local>
2015-07-28 18:32:53 -0500

    Sunshine project Lesson 2. Execute AsyncTask

--- app/src/main/java/mx/saudade/sunshineapp/ForecastFragment.java
+++ app/src/main/java/mx/saudade/sunshineapp/ForecastFragment.java
@@ -3,6 +3,9 @@ package mx.saudade.sunshineapp;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -17,6 +20,12 @@ public class ForecastFragment extends Fragment {
     ListView list;
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
@@ -24,8 +33,27 @@ public class ForecastFragment extends Fragment {
         list = (ListView) rootView.findViewById(R.id.listView_forecast);
         adapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weather);
         list.setAdapter(adapter);
-
         return rootView;
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.forecast_fragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_refresh) {
+            callService();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void callService() {
+        FetchWeatherTask task = new FetchWeatherTask();
+        task.execute();
+    }
 }
