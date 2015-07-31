bf549621541c714fbd3c2b0ffbe0fd98765c8188
Angelica Mendez Vega <angelicamendezvega@fabarca.cameiap.local>
2015-07-31 18:53:27 -0500

    Lesson 3 (before create settings activity)

--- app/src/main/java/mx/saudade/sunshineapp/ForecastFragment.java
+++ app/src/main/java/mx/saudade/sunshineapp/ForecastFragment.java
@@ -1,5 +1,6 @@
 package mx.saudade.sunshineapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,8 +9,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * Created by Angie on 27/07/2015.
@@ -54,7 +57,7 @@ public class ForecastFragment extends Fragment {
         task.execute("94043");
     }
 
-    public void updateAdapter(String[] weather) {
+    public void updateAdapter(final String[] weather) {
         if (weather == null || weather.length == 0) {
             return;
         }
@@ -62,6 +65,21 @@ public class ForecastFragment extends Fragment {
         adapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview,weather);
         list = (ListView) getView().findViewById(R.id.listView_forecast);
         list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String forecast = adapter.getItem(position);
+                startDetailActivity(forecast);
+            }
+        });
+    }
+
+    private void startDetailActivity(String forecast) {
+        Intent intent = new Intent();
+        intent.putExtra(Intent.EXTRA_TEXT, forecast);
+        intent.setClass(getActivity(), DetailActivity.class);
+
+        getActivity().startActivity(intent);
     }
 
 }
